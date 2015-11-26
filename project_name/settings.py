6a6fcc1d169d3921e1a2a24f0e6c493dbaa07625
Ingemar Hjälmstad <iingemar@gmail.com>
2015-11-26 23:40:09 +0100

    Trying to fix whitenoise

--- project_name/settings.py
+++ project_name/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '%glftnam*!k+elz-9#ta)pq)-z8afn*w=-d-x71(+qo@13y=&8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
@@ -55,29 +55,6 @@ ROOT_URLCONF = 'project_name.urls'
 WSGI_APPLICATION = 'project_name.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'database_name',
-        'USER': 'user_name',
-        'PASSWORD': 'xxx',
-        'HOST': '',
-        'PORT': '',
-    }
-}
-"""
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -105,14 +82,3 @@ STATICFILES_DIRS = (
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-
-# If 'DATABASE_URL' exist, then it's on Heroku
-if os.environ.has_key('DATABASE_URL'):
-    # Parse database configuration from $DATABASE_URL
-    DATABASES['default'] =  dj_database_url.config()
-    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
