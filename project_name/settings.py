a0f325cb607478982a09aa9052aadfb5230e21e1
Ingemar Hjälmstad <iingemar@gmail.com>
2015-11-26 23:00:04 +0100

    Add DATABASES again, since Heroku complains

--- project_name/settings.py
+++ project_name/settings.py
@@ -54,6 +54,30 @@ ROOT_URLCONF = 'project_name.urls'
 
 WSGI_APPLICATION = 'project_name.wsgi.application'
 
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'database_name',
+        'USER': 'user_name',
+        'PASSWORD': 'xxx',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+"""
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
