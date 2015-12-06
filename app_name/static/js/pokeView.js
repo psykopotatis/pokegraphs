5498538cc9b59e8fad0ffb9469a81c91068725cd
Ingemar Hjälmstad <iingemar@gmail.com>
2015-12-06 13:40:55 +0100

    More mobile love: add left/right swipe

--- app_name/static/js/pokeView.js
+++ app_name/static/js/pokeView.js
@@ -2,6 +2,7 @@ var drawPieChart = require("./charts/pieChart");
 var drawColumnChart = require("./charts/columnChart");
 var draw3dColumnChart = require("./charts/3d/columnChart");
 var keyboard = require("./keyboard");
+var isMobile = require("./mobile");
 
 const PokeView = Backbone.View.extend({
     el: $('#pokegraphs'),
@@ -77,7 +78,51 @@ const PokeView = Backbone.View.extend({
         this.calculateCenter();
         this.calculateChartHeight();
 
-        $(document).keydown(_.bind(this.onKeyDown, this));
+        console.log('isMobile', isMobile());
+        if (isMobile()) {
+            document.addEventListener('touchstart', _.bind(this.handleTouchStart, this), false);
+            document.addEventListener('touchmove', _.bind(this.handleTouchMove, this), false);
+            this.xDown = null;
+            this.yDown = null;
+        } else {
+            $(document).keydown(_.bind(this.onKeyDown, this));
+        }
+    },
+
+    handleTouchStart: function(evt) {
+        this.xDown = evt.touches[0].clientX;
+        this.yDown = evt.touches[0].clientY;
+    },
+
+    handleTouchMove: function(evt) {
+        if ( ! this.xDown || ! this.yDown ) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        var xDiff = this.xDown - xUp;
+        var yDiff = this.yDown - yUp;
+
+        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+            if ( xDiff > 0 ) {
+                /* left swipe */
+                this.pokeModel.decrement();
+            } else {
+                /* right swipe */
+                this.animatedIncrement();
+            }
+        } else {
+            if ( yDiff > 0 ) {
+                /* up swipe */
+            } else {
+                /* down swipe */
+            }
+        }
+        /* reset values */
+        this.xDown = null;
+        this.yDown = null;
     },
 
     calculateZoom: function() {
@@ -157,16 +202,20 @@ const PokeView = Backbone.View.extend({
                 this.pokeModel.decrement();
                 break;
             case(keyboard.RIGHT):
-                var $el = $('#canvas');
-                $('#charts').empty();
-                $el.addClass('fadeOutRight');
-                $el.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', () => {
-                    $('#canvas').removeClass('fadeOutRight');
-                    this.clearCanvas();
-                    this.pokeModel.increment();
-                });
+                this.animatedIncrement();
                 break;
         }
+    },
+
+    animatedIncrement: function() {
+        var $el = $('#canvas');
+        $('#charts').empty();
+        $el.addClass('fadeOutRight');
+        $el.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', () => {
+            $('#canvas').removeClass('fadeOutRight');
+            this.clearCanvas();
+            this.pokeModel.increment();
+        });
     },
 
     renderColorBlocks: function() {
