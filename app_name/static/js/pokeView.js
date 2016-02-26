fc03cb9315ec3f822b15dc60b15ff38e263160fe
Ingemar Hjälmstad <iingemar@gmail.com>
2016-02-26 21:35:18 +0100

    Add hammerjs for touch events

--- app_name/static/js/pokeView.js
+++ app_name/static/js/pokeView.js
@@ -79,53 +79,39 @@ const PokeView = Backbone.View.extend({
         this.calculateChartHeight();
 
         if (isMobile()) {
-            document.addEventListener('touchstart', _.bind(this.handleTouchStart, this), false);
-            document.addEventListener('touchmove', _.bind(this.handleTouchMove, this), false);
-            this.xDown = null;
-            this.yDown = null;
-            // Update instructions
+            this.handleTouchEvents();
             $('#extraInfo').html('Swipe left/right');
         } else {
             $(document).keydown(_.bind(this.onKeyDown, this));
         }
     },
 
-    handleTouchStart: function(evt) {
-        this.xDown = evt.touches[0].clientX;
-        this.yDown = evt.touches[0].clientY;
-    },
+    handleTouchEvents: function() {
+        var that = this;
+        var panning = false;
+        var hammertime = new Hammer(this.canvas);
 
-    handleTouchMove: function(evt) {
-        if ( ! this.xDown || ! this.yDown ) {
-            return;
-        }
+        hammertime.on('panstart', function() {
+            that.hideExtraInfo();
+        });
 
-        var xUp = evt.touches[0].clientX;
-        var yUp = evt.touches[0].clientY;
-
-        var xDiff = this.xDown - xUp;
-        var yDiff = this.yDown - yUp;
-
-        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-            if ( xDiff > 0 ) {
-                /* left swipe */
-                this.pokeModel.decrement();
-            } else {
-                /* right swipe */
-                this.animatedIncrement();
+        hammertime.on('panleft', function() {
+            if (! panning) {
+                panning = true;
+                that.animatedIncrement();
             }
-        } else {
-            if ( yDiff > 0 ) {
-                /* up swipe */
-            } else {
-                /* down swipe */
-            }
-        }
-        /* reset values */
-        this.xDown = null;
-        this.yDown = null;
+        });
 
-        this.hideExtraInfo();
+        hammertime.on('panright', function() {
+            if (! panning) {
+                panning = true;
+                that.pokeModel.decrement();
+            }
+        });
+
+        hammertime.on('panend', function() {
+            panning = false;
+        });
     },
 
     calculateZoom: function() {
